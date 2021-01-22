c156c8a97a113db6dc63de3c482b8130d34e51d5
Petar Petrov <petar.dragov.petrov@gmail.com>
2021-01-22 09:48:01 +0100

    Add project files.

---
+++ 1. Diagonal Difference/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Linq;
+
+namespace _10._Radioactive_Mutant_Vampire_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfMatrix = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[,];
+
+            for (int row = 0; row < sizeOfMatrix; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+
+            }
+
+        }
+    }
+}
